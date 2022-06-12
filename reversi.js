7123c9e228d089e8da911a11cf64cfdb552823fe
Ryota Wakizaka <rwakizaka7@yahoo.co.jp>
2022-06-13 03:09:31 +0900

    ピースを置ける場所に置けるようにする

--- reversi.js
+++ reversi.js
@@ -1,13 +1,14 @@
-var screenInfo = {
+let screenInfo = {
     width: window.innerWidth <= window.innerHeight
         ? window.innerWidth : window.innerHeight,
-    height: window.innerHeight,
+    height: window.innerHeight <= window.innerWidth
+        ? window.innerHeight : window.innerWidth,
     offsetRate: {
         x: 0.05, y: 0.05
     }
 };
 
-var reversiBoardInfo = {
+let reversiBoardInfo = {
     key: 'reversi_board',
     url: 'assets/reversi_board_505x505.svg',
     width: 505,
@@ -17,36 +18,35 @@ var reversiBoardInfo = {
     }
 };
 
-var reversiPieceBlankInfo = {
+let reversiPieceBlankInfo = {
     key: 'reversi_piece_blank',
     url: 'assets/reversi_piece_blank_61x61.svg',
     width: 61,
     height: 61
 }
 
-var reversiPieceBlackInfo = {
+let reversiPieceBlackInfo = {
     key: 'reversi_piece_black',
     url: 'assets/reversi_piece_black_61x61.svg',
     width: 61,
     height: 61
 }
 
-var reversiPieceWhiteInfo = {
+let reversiPieceWhiteInfo = {
     key: 'reversi_piece_white',
     url: 'assets/reversi_piece_white_61x61.svg',
     width: 61,
     height: 61
 }
 
-var reversiGaming = {
+let reversiGaming = {
     pieceLineNum: 8,
     images: {
         pieces: new Array(this.pieceLineNum),
-        getPieceIndexs: function(image) {
+        getPieceIndex: function(image) {
             for (let rowIdx in this.pieces) {
                 for (let colIdx in this.pieces[rowIdx]) {
                     let images = this.pieces[rowIdx][colIdx];
-                    //if (images.filter(imageSet => imageSet.blank == image).length == 1) {
                     if (images.blank == image) {
                         return {
                             rowIdx: rowIdx, colIdx: colIdx
@@ -54,16 +54,136 @@ var reversiGaming = {
                     }
                 }
             }
+        },
+        reflectPiece: function(states) {
+            for (let rowIdx in this.pieces) {
+                for (let colIdx in this.pieces[rowIdx]) {
+                    let state = states.boardMarix[rowIdx][colIdx];
+                    let piece = this.pieces[rowIdx][colIdx];
+                    switch (state) {
+                        case 'e': // Empty
+                            piece.black.setVisible(false);
+                            piece.white.setVisible(false);
+                            break;
+                        case 'b': // Black
+                            piece.black.setVisible(true);
+                            piece.white.setVisible(false);
+                            break;
+                        case 'w': // White
+                            piece.black.setVisible(false);
+                            piece.white.setVisible(true);
+                            break;
+                    }
+                }
+            }
         }
     },
     states: {
+        turnWhite: false,
         boardMarix: new Array(this.pieceLineNum),
         initStates: function () {
+            this.turnWhite = false;
             reversiGaming.initBoardMatrix(this.boardMarix, "e");
             this.boardMarix[3][3] = "w";
             this.boardMarix[3][4] = "b";
             this.boardMarix[4][3] = "b";
             this.boardMarix[4][4] = "w";
+        },
+        selectPiece: function(rowIdx, colIdx) {
+            let gaming = reversiGaming;
+            let states = gaming.states;
+
+            let reverceList = this.getObtainablePiece(rowIdx, colIdx);
+            if (reverceList.length == 0) {
+                return;
+            }
+
+            this.boardMarix[rowIdx][colIdx] = !this.turnWhite ? 'b' : 'w';
+
+            for (let i in reverceList) {
+                let rowIdx = reverceList[i].rowIdx;
+                let colIdx = reverceList[i].colIdx;
+
+                if (this.boardMarix[rowIdx][colIdx] == 'b') {
+                    this.boardMarix[rowIdx][colIdx] = 'w'
+                } else if (this.boardMarix[rowIdx][colIdx] == 'w') {
+                    this.boardMarix[rowIdx][colIdx] = 'b'
+                }
+            }
+
+            gaming.images.reflectPiece(states);
+            states.turnWhite = !states.turnWhite
+        },
+        getObtainablePiece: function(rowIdx, colIdx) {
+            let gaming = reversiGaming;
+
+            let directions = [
+                {directionRowIdx: -1, directionColIdx: 0}, // Upper Middle
+                {directionRowIdx: -1, directionColIdx: 1}, // Upper Right
+                {directionRowIdx: 0, directionColIdx: 1}, // Middle Right
+                {directionRowIdx: 1, directionColIdx: 1}, // Lower Right
+                {directionRowIdx: 1, directionColIdx: 0}, // Lower Middle
+                {directionRowIdx: 1, directionColIdx: -1}, // Lower Left
+                {directionRowIdx: 0, directionColIdx: -1}, // Middle Left
+                {directionRowIdx: -1, directionColIdx: -1} // Upper Left
+            ];
+
+            var confirmedReverceList = [];
+
+            if (this.boardMarix[rowIdx][colIdx] != 'e') {
+                return confirmedReverceList;
+            }
+
+            for (let i in directions) {
+                let directionRowIdx = directions[i].directionRowIdx;
+                let directionColIdx = directions[i].directionColIdx;
+
+                var first = true;
+                var reverceList = [];
+
+                var currentRowIdx = Number(rowIdx);
+                var currentColIdx = Number(colIdx);
+
+                while (true) {
+                    currentRowIdx = currentRowIdx + directionRowIdx;
+                    if (currentRowIdx < 0 || currentRowIdx >= gaming.pieceLineNum) {
+                        break;
+                    }
+    
+                    currentColIdx = currentColIdx + directionColIdx;
+                    if (currentColIdx < 0 || currentColIdx >= gaming.pieceLineNum) {
+                        break;
+                    }
+    
+                    if (first) {
+                        // Opponent's stone
+                        if (!this.turnWhite && this.boardMarix[currentRowIdx][currentColIdx] != 'w'
+                            || this.turnWhite && this.boardMarix[currentRowIdx][currentColIdx] != 'b') {
+                            break;
+                        } else {
+                            reverceList.push({
+                                rowIdx: currentRowIdx, colIdx: currentColIdx
+                            });
+                            first = false
+                        }
+                    } else {
+                        // Own's stone
+                        if (!this.turnWhite && this.boardMarix[currentRowIdx][currentColIdx] == 'b'
+                            || this.turnWhite && this.boardMarix[currentRowIdx][currentColIdx] == 'w') {
+                            confirmedReverceList = confirmedReverceList.concat(reverceList);
+                            break;
+                        } else if (this.boardMarix[currentRowIdx][currentColIdx] == 'e') {
+                            break;
+                        } else {
+                            reverceList.push({
+                                rowIdx: currentRowIdx, colIdx: currentColIdx
+                            });
+                        }
+                    }
+                }
+            }
+
+            return confirmedReverceList;
         }
     },
     initBoardMatrix: function(matrix, value) {
@@ -85,8 +205,8 @@ function setScreenRateFuncs(info) {
     };
 
     info['seekScreenFitZoomRate'] = function (screenInfo) {
-        var offset = this.getOffset(screenInfo);
-        var offsetY = offset.y
+        let offset = this.getOffset(screenInfo);
+        let offsetY = offset.y
         this['zoomRate'] = (screenInfo.width - offsetY * 2) / this.width;
     };
 
@@ -125,15 +245,15 @@ function setBoardPieceRateFuncs(info) {
     }
 
     info['seekScreenFitZoomRate'] = function (screenInfo) {
-        var offsetX = this.getOffset(screenInfo).x
+        let offsetX = this.getOffset(screenInfo).x
             + this.getBorderOffset().x;
         this['zoomRate'] = (screenInfo.width - offsetX * 2
             ) / reversiGaming.pieceLineNum / this.width;
     };
 
     info['getBoardIndexOffset'] = function (screenInfo, colIdx, rowIdx) {
-        var zoomSize = this.getZoomSize();
-        var borderOffset = this.getBorderOffset();
+        let zoomSize = this.getZoomSize();
+        let borderOffset = this.getBorderOffset();
         return {
             x: screenInfo.width * screenInfo.offsetRate.x
                 + zoomSize.width * colIdx
@@ -148,7 +268,7 @@ setBoardPieceRateFuncs(reversiPieceBlankInfo);
 setBoardPieceRateFuncs(reversiPieceBlackInfo);
 setBoardPieceRateFuncs(reversiPieceWhiteInfo);
 
-var game = new Phaser.Game({
+let game = new Phaser.Game({
     type: Phaser.WEBGL,
     width: screenInfo.width,
     height: screenInfo.height,
@@ -159,7 +279,7 @@ var game = new Phaser.Game({
                 reversiPieceWhiteInfo]
             .forEach(info => {
                 info.seekScreenFitZoomRate(screenInfo);
-                var size = info.getZoomSize();
+                let size = info.getZoomSize();
                 console.log(size);
                 this.load.svg(info.key, info.url, {
                     width: size.width,
@@ -168,42 +288,46 @@ var game = new Phaser.Game({
             })
         },
         create: function() {
-            var gaming = reversiGaming;
-            var states = gaming.states;
+            let gaming = reversiGaming;
+            let states = gaming.states;
             states.initStates();
 
             {
-                var info = reversiBoardInfo;
-                var center = info.getCenter();
-                var offset = info.getOffset(screenInfo);
+                let info = reversiBoardInfo;
+                let center = info.getCenter();
+                let offset = info.getOffset(screenInfo);
                 this.add.image(
                     center.x + offset.x,
                     center.y + offset.y,
                     info.key);
             }
 
-            var pieces = gaming.images.pieces;
+            let images = gaming.images;
+            let pieces = images.pieces;
             gaming.initBoardMatrix(pieces, null);
             for (let rowIdx in pieces) {
                 for (let colIdx in pieces[rowIdx]) {
-                    var images = {
+                    let images = {
                         blank: [],
                         black: [],
                         white: []
                     };
 
-                    [{info: reversiPieceBlankInfo, piece: images.blank, visible: true},
-                        {info: reversiPieceBlackInfo, piece: images.black, visible: false},
-                        {info: reversiPieceWhiteInfo, piece: images.white, visible: false}]
+                    [{info: reversiPieceBlankInfo, piece: images.blank, visible: true, interactive: true},
+                        {info: reversiPieceBlackInfo, piece: images.black, visible: false, interactive: false},
+                        {info: reversiPieceWhiteInfo, piece: images.white, visible: false, interactive: false}]
                         .forEach(t => {
-                            var info = t.info;
-                            var center = info.getCenter();
-                            var offset = info.getBoardIndexOffset(screenInfo, colIdx, rowIdx);
+                            let info = t.info;
+                            let center = info.getCenter();
+                            let offset = info.getBoardIndexOffset(screenInfo, colIdx, rowIdx);
     
                             t.piece.push(this.add.image(
                                 center.x + offset.x,
                                 center.y + offset.y,
-                                info.key).setInteractive());
+                                info.key));
+                            if (t.interactive) {
+                                t.piece[0].setInteractive()
+                            }
                             t.piece[0].setVisible(t.visible);
                         });
 
@@ -214,34 +338,19 @@ var game = new Phaser.Game({
                 }
             }
 
-            for (let rowIdx in pieces) {
-                for (let colIdx in pieces[rowIdx]) {
-                    var state = states.boardMarix[rowIdx][colIdx];
-                    var piece = pieces[rowIdx][colIdx];
-                    switch (state) {
-                        case 'e':
-                            piece.black.setVisible(false);
-                            piece.white.setVisible(false);
-                            break;
-                        case 'b':
-                            piece.black.setVisible(true);
-                            piece.white.setVisible(false);
-                            break;
-                        case 'w':
-                            piece.black.setVisible(false);
-                            piece.white.setVisible(true);
-                            break;
-                    }
-                }
-            }
+            images.reflectPiece(states);
         
             // this.input.on('gameobjectdown', (pointer, gameobject) => {
             //     //console.log('d');
             // });
         
             this.input.on('gameobjectup', (pointer, gameobject) => {
-                let index = gaming.images.getPieceIndexs(gameobject);
+                let gaming = reversiGaming;
+                let states = gaming.states;
+                let index = gaming.images.getPieceIndex(gameobject);
                 console.log(index);
+                states.selectPiece(index.rowIdx, index.colIdx);
+                console.log(states.boardMarix);
                 //console.log('u');
             });
         },
